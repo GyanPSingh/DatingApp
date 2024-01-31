ae0e6611ad31077ebf7c5e88e3a6e6e8fc3f23e5
GPS009 <gp.singh@diyarme.com>
2024-01-31 11:44:24 +0530

    first commit

---
+++ API/WebAPI/Program.cs
@@ -0,0 +1,29 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<DataContext>(opt=>
+{
+opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
