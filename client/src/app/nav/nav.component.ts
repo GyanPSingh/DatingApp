fc8d2cfcc2bbb9b4458a56fa09dc0567fe151701
GPS009 <GPS009@diyarme.com>
2024-02-08 23:06:56 +0530

    Login and Set user detail in local storage

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,21 +10,33 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn=false;
+  currentUser$: Observable<User | null> = of(null);
 
   constructor(private accountService: AccountService) { }
   ngOnInit(): void {
-
+    this.currentUser$=this.accountService.currentUser$;
   }
+
+  // getCurrentUser() {
+  //   this.accountService.currentUser$.subscribe({
+  //     next: user => this.loggedIn = !!user,
+  //     error: error => console.log(error)
+  //   })
+  // }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next:response =>{
+      next: response => {
         console.log(response);
-        this.loggedIn=true;
+        // this.loggedIn = true;
       },
-      error: error=>{console.log(error)}
+      error: error => { console.log(error) }
     })
     //console.log(this.model);
+  }
+
+  logout() {
+    this.accountService.logout();
+    // this.loggedIn = false;
   }
 }
